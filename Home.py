fb8b066830a04b6b2e2697e3963e23acaae4ae81
hollarme <33898017+hollarme@users.noreply.github.com>
2025-01-04 23:40:57 +0000

    date selection for db

--- Home.py
+++ Home.py
@@ -25,114 +25,117 @@ if 'dt' not in st.session_state:
 
 rec, ana = st.columns([0.7,0.3])
 
-with ana:
-    with st.container(border=True):
-        d = st.date_input("What record year are you interested in?", datetime.today())
+try:
 
-        period = st.radio(
-            "Over which period?",
-            ["***Q1***", "***Q2***", "***Q3***", "***Q4***", "***H1***", "***H2***", "***Y***"],
-            captions = ["Jan-Mar", "Apr-Jun", "Jul-Sept", "Oct-Dec", "Jan-Jun", "Jul-Dec", "Jan-Dec"], horizontal=True)
+    with ana:
+        with st.container(border=True):
+            d = st.date_input("What record year are you interested in?", None)
 
-        record = f"record_{d.year}_{period.replace('*','')}"
+            period = st.radio(
+                "Over which period?",
+                ["***Q1***", "***Q2***", "***Q3***", "***Q4***", "***H1***", "***H2***", "***Y***"],
+                captions = ["Jan-Mar", "Apr-Jun", "Jul-Sept", "Oct-Dec", "Jan-Jun", "Jul-Dec", "Jan-Dec"], horizontal=True)
 
-        st.session_state['collection'] = record
+            record = f"record_{d.year}_{period.replace('*','')}"
 
-if 'data_df' not in st.session_state:
-    st.session_state["data_df"] = pd.DataFrame(get_data('FarmSetup_ProductList'))
+            st.session_state['collection'] = record
 
-if 'data_df_cat' not in st.session_state:
-    st.session_state["data_df_cat"] = pd.DataFrame(get_data('FarmSetup_ProductCat'))
-    
-if 'exp_data_df_cat' not in st.session_state:
-    st.session_state["exp_data_df_cat"] = pd.DataFrame(get_data('FarmSetup_ExpensesCat'))
+    if 'data_df' not in st.session_state:
+        st.session_state["data_df"] = pd.DataFrame(get_data('FarmSetup_ProductList'))
 
-if 'expx_data_df' not in st.session_state:
-    st.session_state['expx_data_df'] = pd.DataFrame(get_data('FarmExpenses'))
+    if 'data_df_cat' not in st.session_state:
+        st.session_state["data_df_cat"] = pd.DataFrame(get_data('FarmSetup_ProductCat'))
+        
+    if 'exp_data_df_cat' not in st.session_state:
+        st.session_state["exp_data_df_cat"] = pd.DataFrame(get_data('FarmSetup_ExpensesCat'))
 
-if 'note_data_df' not in st.session_state:
-    st.session_state['note_data_df'] = pd.DataFrame(get_data('FarmNotes'))
+    if 'expx_data_df' not in st.session_state:
+        st.session_state['expx_data_df'] = pd.DataFrame(get_data('FarmExpenses'))
 
-if 'opr_data_df' not in st.session_state:
-    st.session_state['opr_data_df'] = pd.DataFrame(get_data('FarmOperation'))
+    if 'note_data_df' not in st.session_state:
+        st.session_state['note_data_df'] = pd.DataFrame(get_data('FarmNotes'))
 
-if 'op_data_df' not in st.session_state:
-    st.session_state['op_data_df'] = pd.DataFrame(get_data('FarmOperationList'))
+    if 'opr_data_df' not in st.session_state:
+        st.session_state['opr_data_df'] = pd.DataFrame(get_data('FarmOperation'))
 
-if 'lab_data_df' not in st.session_state:
-    st.session_state['lab_data_df'] = pd.DataFrame(get_data('FarmLabourer'))
+    if 'op_data_df' not in st.session_state:
+        st.session_state['op_data_df'] = pd.DataFrame(get_data('FarmOperationList'))
 
-if 'inc_data_df' not in st.session_state:
-    st.session_state['inc_data_df'] = pd.DataFrame(get_data('FarmIncome'))
+    if 'lab_data_df' not in st.session_state:
+        st.session_state['lab_data_df'] = pd.DataFrame(get_data('FarmLabourer'))
 
-if 'tran_data_df' not in st.session_state:
-    st.session_state["tran_data_df"] = pd.DataFrame(get_data('FarmTransaction'))
-    
-with rec:    
-    with st.container(border=True):
-        home_tab, income_tab, expenses_tab, note_tab = st.tabs(['Home', 'Income', 'Expenses', 'Note'])
+    if 'inc_data_df' not in st.session_state:
+        st.session_state['inc_data_df'] = pd.DataFrame(get_data('FarmIncome'))
 
-        with home_tab:
-            pass
+    if 'tran_data_df' not in st.session_state:
+        st.session_state["tran_data_df"] = pd.DataFrame(get_data('FarmTransaction'))
+        
+    with rec:    
+        with st.container(border=True):
+            home_tab, income_tab, expenses_tab, note_tab = st.tabs(['Home', 'Income', 'Expenses', 'Note'])
 
-        with income_tab:
-            b_df = st.session_state["inc_data_df"]
-            xl = b_df['Category'].tolist()
-            xl.insert(0,"")
-            category = st.selectbox("select the expenses category", set(xl), key='inccC')
-            xl = b_df.loc[b_df['Category']==category]['Product'].tolist()
-            xl.insert(0,"")
-            prod = st.selectbox("select the product", set(xl), key='inccP')
-            xl = b_df.loc[(b_df['Category']==category)&(b_df['Product']==prod)]['Type'].tolist()
-            xl.insert(0,"")
-            types = st.selectbox("select the product type if available", set(xl), key='inccT')
-            
-            label = ""
-            
-            if category:
-                b_df = b_df[b_df['Category'].str.contains(category, na=False, regex=False)]
-                label = ""#":black_small_square:" + category + ":red[Cost]" + ":black_small_square:" 
-            if prod:
-                b_df = b_df[b_df['Product'].str.contains(prod, na=False, regex=False)]
-                label = ""#":black_small_square:" + prod + ":red[Cost]" + ":black_small_square:" 
-            if types:
-                b_df = b_df[b_df['Type'].str.contains(types, na=False, regex=False)]
-                label =  ""#":black_small_square:" + types + ":red[Cost]" + ":black_small_square:"
+            with home_tab:
+                pass
+
+            with income_tab:
+                b_df = st.session_state["inc_data_df"]
+                xl = b_df['Category'].tolist()
+                xl.insert(0,"")
+                category = st.selectbox("select the expenses category", set(xl), key='inccC')
+                xl = b_df.loc[b_df['Category']==category]['Product'].tolist()
+                xl.insert(0,"")
+                prod = st.selectbox("select the product", set(xl), key='inccP')
+                xl = b_df.loc[(b_df['Category']==category)&(b_df['Product']==prod)]['Type'].tolist()
+                xl.insert(0,"")
+                types = st.selectbox("select the product type if available", set(xl), key='inccT')
                 
-                                       
-            col1, col2, col3 = st.columns(3)
-            
-            col2.metric(label=label, value=prettify(b_df['Total Price(N)'].sum()))
-
-        with expenses_tab:
-            b_df = st.session_state["expx_data_df"]#st.session_state["exp_data_df_cat"]
-            xl = b_df['Category'].tolist()
-            xl.insert(0,"")
-            category = st.selectbox("select the expenses category", set(xl))
-            xl = b_df.loc[b_df['Category']==category]['Expenses'].tolist()
-            xl.insert(0,"")
-            expenses = st.selectbox("select the expenses", set(xl))
-            xl = b_df.loc[(b_df['Category']==category)&(b_df['Expenses']==expenses)]['Type'].tolist()
-            xl.insert(0,"")
-            types = st.selectbox("select the expenses type if available", set(xl))
-            
-            label = ""
-            
-            if category:
-                b_df = b_df[b_df['Category'].str.contains(category, na=False, regex=False)]
-                label = ""#":black_small_square:" + category + ":red[Cost]" + ":black_small_square:" 
-            if expenses:
-                b_df = b_df[b_df['Expenses'].str.contains(expenses, na=False, regex=False)]
-                label = "" #":black_small_square:" + expenses + ":red[Cost]" + ":black_small_square:" 
-            if types:
-                b_df = b_df[b_df['Type'].str.contains(types, na=False, regex=False)]
-                label = ""#":black_small_square:" + types + ":red[Cost]" + ":black_small_square:"
+                label = ""
                 
-                                       
-            col1, col2, col3 = st.columns(3)
-            
-            col2.metric(label=label, value=prettify(b_df['Amount'].sum()))
+                if category:
+                    b_df = b_df[b_df['Category'].str.contains(category, na=False, regex=False)]
+                    label = ""#":black_small_square:" + category + ":red[Cost]" + ":black_small_square:" 
+                if prod:
+                    b_df = b_df[b_df['Product'].str.contains(prod, na=False, regex=False)]
+                    label = ""#":black_small_square:" + prod + ":red[Cost]" + ":black_small_square:" 
+                if types:
+                    b_df = b_df[b_df['Type'].str.contains(types, na=False, regex=False)]
+                    label =  ""#":black_small_square:" + types + ":red[Cost]" + ":black_small_square:"
+                    
+                                        
+                col1, col2, col3 = st.columns(3)
+                
+                col2.metric(label=label, value=prettify(b_df['Total Price(N)'].sum()))
 
-        with note_tab:
-            pass
+            with expenses_tab:
+                b_df = st.session_state["expx_data_df"]#st.session_state["exp_data_df_cat"]
+                xl = b_df['Category'].tolist()
+                xl.insert(0,"")
+                category = st.selectbox("select the expenses category", set(xl))
+                xl = b_df.loc[b_df['Category']==category]['Expenses'].tolist()
+                xl.insert(0,"")
+                expenses = st.selectbox("select the expenses", set(xl))
+                xl = b_df.loc[(b_df['Category']==category)&(b_df['Expenses']==expenses)]['Type'].tolist()
+                xl.insert(0,"")
+                types = st.selectbox("select the expenses type if available", set(xl))
+                
+                label = ""
+                
+                if category:
+                    b_df = b_df[b_df['Category'].str.contains(category, na=False, regex=False)]
+                    label = ""#":black_small_square:" + category + ":red[Cost]" + ":black_small_square:" 
+                if expenses:
+                    b_df = b_df[b_df['Expenses'].str.contains(expenses, na=False, regex=False)]
+                    label = "" #":black_small_square:" + expenses + ":red[Cost]" + ":black_small_square:" 
+                if types:
+                    b_df = b_df[b_df['Type'].str.contains(types, na=False, regex=False)]
+                    label = ""#":black_small_square:" + types + ":red[Cost]" + ":black_small_square:"
+                    
+                                        
+                col1, col2, col3 = st.columns(3)
+                
+                col2.metric(label=label, value=prettify(b_df['Amount'].sum()))
 
+            with note_tab:
+                pass
+except AttributeError:
+    st.warning('Enter the year and period of interest')
